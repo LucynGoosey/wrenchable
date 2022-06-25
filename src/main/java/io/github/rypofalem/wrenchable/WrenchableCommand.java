6c45e1a62ea1a72de0b31c8c0e733c9c276eef41
rypofalem <rypofalem@gmail.com>
2022-06-24 19:40:15 -0500

    Remove the debugplatform subcommand.
    It was only availible to OPs and it had a threatening confirmation system but there's just no justification to have that function sitting around a live server. Can move that to a side plugin for testing.

--- src/main/java/io/github/rypofalem/wrenchable/WrenchableCommand.java
+++ src/main/java/io/github/rypofalem/wrenchable/WrenchableCommand.java
@@ -1,7 +1,5 @@
 package io.github.rypofalem.wrenchable;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.data.Directional;
 import org.bukkit.block.data.Orientable;
@@ -12,12 +10,10 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -59,36 +55,6 @@ public class WrenchableCommand implements CommandExecutor {
                     throw new RuntimeException(e);
                 }
                 return true;
-
-
-            // create a platform of wrenchable blocks
-            case "debugplatform":
-                if(!(sender instanceof Player)){
-                    sender.sendMessage("Only players can execute this command.");
-                    return true;
-                }
-                if(args.length < 2 || !args[1].equals("isweariamnotatotaldumbass")) {
-                    sender.sendMessage("This command is destructive. It will replace hundreds of blocks.)");
-                    sendMessageDelay(sender, "It may even place invalid/corrupted blocks.", 3);
-                    sendMessageDelay(sender, "Do NOT use this on server that is not disposable.", 6);
-                    sendMessageDelay(sender, "Type \"/wrenchable debugplatform isweariamnotatotaldumbass\" to confirm", 9);
-                    return true;
-                }
-                Location feet = ((Player)sender).getLocation();
-                Iterator<Material> blocks = validBlocks().iterator();
-                for(int x = feet.getBlockX()-50; x < feet.getBlockX() + 50; x++){
-                    for(int z = feet.getBlockZ()-50; z < feet.getBlockZ() + 50; z++){
-                        feet.getWorld().getBlockAt(x, feet.getBlockY(), z).setType(Material.STONE);
-                        for(int yOffset = 0; yOffset < 5; yOffset++){
-                            feet.getWorld().getBlockAt(x, 1 + yOffset + feet.getBlockY(), z).setType(Material.AIR);
-                        }
-                        if((x%5) == 0 && z % 5 == 0 && blocks.hasNext())
-                            feet.getWorld().getBlockAt(x, 1 + feet.getBlockY(), z).setType(blocks.next());
-
-                    }
-                }
-                return true;
-
             default: return false;
         }
     }
@@ -98,9 +64,5 @@ public class WrenchableCommand implements CommandExecutor {
                 m -> !m.name().contains("LEGACY") && m.isBlock()
                 && (m.createBlockData() instanceof Directional || m.createBlockData() instanceof Orientable || m.createBlockData() instanceof Rotatable)
         );
-    }
-
-    private void sendMessageDelay(CommandSender reciever, String message, long seconds){
-        Bukkit.getScheduler().runTaskLater(plugin, () -> reciever.sendMessage(message), seconds * 20);
     }
 }
